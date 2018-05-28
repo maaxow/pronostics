809ccce03555385d83b24e2ef42afe557f330dbf
Axel-Verbrouck <axelverbrouck@hotmail.fr>
2018-05-28 14:21:23 +0200

    maquettage bracket phases finales

--- WebContent/js/arborescence.js
+++ WebContent/js/arborescence.js
@@ -1,6 +1,6 @@
 var c = document.getElementById("myCanvas");
 	var ctx = c.getContext("2d");
-	var link = function(ctx, x, y){
+	var huitiemeLink = function(ctx, x, y){
 	    ctx.moveTo(x,y);
 	    ctx.lineTo(x+15,y);
 
@@ -18,6 +18,60 @@ var c = document.getElementById("myCanvas");
 	    ctx.stroke();
 	}
 
+	var quartLink = function(ctx, x, y){
+	    ctx.moveTo(x,y);
+	    ctx.lineTo(x+15,y);
+
+	    ctx.moveTo(x+15,y);
+	    ctx.lineTo(x+15,y+150);
+
+	    ctx.moveTo(x,y+150);
+	    ctx.lineTo(x+15,y+150);
+
+	    ctx.moveTo(x+15,y+150);
+	    ctx.lineTo(x+15,y+25);
+
+	    ctx.moveTo(x+15,y+75);
+	    ctx.lineTo(x+30,y+75);
+	    ctx.stroke();
+	}
+
+	var demiLink = function(ctx, x, y){
+	    ctx.moveTo(x,y);
+	    ctx.lineTo(x+15,y);
+
+	    ctx.moveTo(x+15,y);
+	    ctx.lineTo(x+15,y+300);
+
+	    ctx.moveTo(x,y+300);
+	    ctx.lineTo(x+15,y+300);
+
+	    ctx.moveTo(x+15,y+300);
+	    ctx.lineTo(x+15,y+25);
+
+	    ctx.moveTo(x+15,y+150);
+	    ctx.lineTo(x+30,y+150);
+	    ctx.stroke();
+	}
+
+	var finaleLink = function(ctx, x, y){
+	    ctx.moveTo(x,y);
+	    ctx.lineTo(x+15,y);
+
+	    ctx.moveTo(x+15,y);
+	    ctx.lineTo(x+15,y+600);
+
+	    ctx.moveTo(x,y+600);
+	    ctx.lineTo(x+15,y+600);
+
+	    ctx.moveTo(x+15,y+600);
+	    ctx.lineTo(x+15,y+25);
+
+	    ctx.moveTo(x+15,y+300);
+	    ctx.lineTo(x+250,y+300);
+	    ctx.stroke();
+	}
+
 	var rectangle = function(ctx, x, y){
 		ctx.rect(x, y, 220, 40);
 		ctx.stroke();
@@ -25,8 +79,11 @@ var c = document.getElementById("myCanvas");
 
 	rectX = 20;
 	rectY = 20;
+	linkX = 240;
+	linkY = 40;
 
-	for(index = 1; index < 9; index ++){
+	//rectangles des 8e de finale
+	for(index = 1; index < 17; index ++){
 		if(index % 2 == 0){
 			rectY += 50;
 			rectangle(ctx, rectX, rectY);
@@ -37,11 +94,56 @@ var c = document.getElementById("myCanvas");
 			rectangle(ctx, rectX, rectY);
 		}
 	}
-
-	linkX = 240;
-	linkY = 40;
-
-	for(index = 1; index < 5; index ++){
-		link(ctx, linkX, linkY);
+	//liens des 8e de finales
+	for(index = 1; index < 9; index ++){
+		huitiemeLink(ctx, linkX, linkY);
 		linkY += 150;
 	}
+
+	rectX = 270;
+	rectY = 45;
+	linkX = 490;
+	linkY = 65;
+
+	//rectangles des quarts de finale
+	for(index = 1; index < 9; index ++){
+		rectangle(ctx, rectX, rectY);
+		rectY += 150;
+	}
+	//liens des quarts de finale
+	for(index = 1; index < 5; index ++){
+		quartLink(ctx, linkX, linkY);
+		linkY += 300;
+	}
+
+		rectX = 520;
+		rectY = 120;
+		linkX = 740;
+		linkY = 140;
+
+	//rectangles des quarts de finale
+	for(index = 1; index < 5; index ++){
+		rectangle(ctx, rectX, rectY);
+		rectY += 300;
+	}
+	//liens des quarts de finale
+	for(index = 1; index < 3; index ++){
+		demiLink(ctx, linkX, linkY);
+		linkY += 600;
+	}
+
+		rectX = 770;
+		rectY = 270;
+		linkX = 740;
+		linkY = 140;
+
+	//rectangles des quarts de finale
+	for(index = 1; index < 3; index ++){
+		rectangle(ctx, rectX, rectY);
+		rectY += 600;
+	}
+	//liens des quarts de finale
+/*	for(index = 1; index < 3; index ++){*/
+		finaleLink(ctx, 990, 290);
+		/*linkY += 600;
+	}*/
