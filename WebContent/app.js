a54a7fb6bbf2d2817390d63ed08be09b2432bcd0
maaxow <maxime.rose@hotmail.fr>
2018-06-04 23:01:15 +0200

    petite amélioration de l'ihm

--- WebContent/app.js
+++ WebContent/app.js
@@ -1,19 +1,20 @@
 (function(){
 	'use strict';
-	var app = angular.module('pronostic',['pronostic.directives.toolbar']);
+	var app = angular.module('pronostic',['ui.router'/*, 'pronostic.directives.toolbar'*/]);
 
-	app.config(['$locationProvider','$stateProvider','$urlRouterProvider', 'paths', function($locationProvider, $stateProvider, $urlRouterProvider, paths) {
+	app.config(['$locationProvider','$stateProvider','$urlRouterProvider', 
+		function($locationProvider, $stateProvider, $urlRouterProvider) {
 		$stateProvider
 		.state('home', {
 			url: "/",
-			templateUrl: paths.views + '/index.html',
+			templateUrl: 'index.html',
 			controller : 'HomeController'
 		})
 		.state('home.finale', {
-			url: "finale",
+			url: "/finale",
 			views : {
 				'main@home' : {
-					templateUrl: paths.views + '/finale.html',
+					templateUrl: 'components/finale/finale.html',
 					controller : 'FinaleController'
 				}
 			}
@@ -35,10 +36,22 @@
 		
 
 		// For invalid route
-		$urlRouterProvider.otherwise('/issue');
+//		$urlRouterProvider.otherwise('/');
 		// use the HTML5 History API
 		$locationProvider.html5Mode(true);
 
+	}])
+	.controller("HomeController", ['$scope', function($scope){
+		console.log("HomeController");
+		$scope.message = "Home conntroller";
+	}])
+	.directive("pronosticToolbar",[function(){
+				return {
+					templateUrl : 'directives/toolbar/toolbar.html',
+					controller : ['$scope',
+							function($scope){
+						$scope.message = "Test directive toolbar";
+					}]
+				}
 	}]);
 })();
-	
