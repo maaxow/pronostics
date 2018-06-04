d3f86f314e80f6c1d14367048c94a0e03b599ec4
Axel-Verbrouck <axelverbrouck@hotmail.fr>
2018-06-04 13:42:20 +0200

    init ihm

---
+++ WebContent/directives/toolbar/toolbar.js
@@ -0,0 +1,13 @@
+(function(){
+	'use strict';
+	var directivesModule = angular.module('pronostic.directives.toolbar', [])
+			.directive("pronosticToolbar",[function(){
+				return {
+					templateUrl : 'directives/toolbar/toolbar.html',
+					controller : ['$scope',
+							function($scope){
+						$scope.message = "Test directive toolbar";
+					}]
+				}
+			}]);
+});
